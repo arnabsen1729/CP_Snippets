cfc33919fb49c3a9cf6ccad39613d62b9d0396c0
Mr-DareDevil <arnabsen1729@gmail.com>
2019-07-09 09:52:52 +0530

    "Template added"

---
+++ template.cpp
@@ -0,0 +1,70 @@
+/*
+ * Author: $%U%$
+ * Time: $%Y%$-$%M%$-$%D%$
+**/
+
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iomanip>
+#include <stdio.h>
+#include <cmath>
+
+using namespace std;
+
+//typedef
+typedef string STR;
+typedef long long LL;
+typedef long double LD;
+typedef unsigned long long ULL;
+typedef vector<int> VI;
+typedef vector<LL> VLL;
+typedef vector<ULL> VULL;
+typedef vector<STR> VS;
+typedef pair<int,int> PII;
+typedef pair<LL,LL> PLL;
+typedef vector<PII> VII;
+typedef map<int,int> MII;
+typedef map<LL,LL> MLL;
+typedef set<STR> SS;
+typedef set<int> SI;
+typedef set<LL> SLL;
+
+//symbolic const
+#define MOD (LL)1000000007
+#define MAXN 1e9
+#define SIZE 10000
+
+//STL keywords
+#define FF first
+#define SS second
+#define PB push_back
+#define POB pop_back
+#define PF push_front
+#define POF pop_front
+#define MKP make_pair
+#define all(v) v.begin(), v.end()
+
+//functions
+#define dec(n) cout<<fixed<<setprecision(n);
+#define f(i, n)   for(LL i=0; i<n; i++)
+#define fa(i,a,n) for(LL i=a; i<n; i++)
+#define UB(v,x)	upper_bound(all(v),x)		//x>a[<-pos]
+#define LB(v,x)	lower_bound(all(v),x)		//x<=a[pos->]
+
+//IO
+#define sync ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define test int t;cin>>t;while(t--){solve();}
+
+template <class C> void setmin(C &a,C b){if(a>b)a=b;}
+template <class C> void setmax(C &a,C b){if(a<b)a=b;}
+
+void solve() {
+
+}
+
+int main() {
+    sync;
+    //test;
+    solve();
+    return 0;
+}
